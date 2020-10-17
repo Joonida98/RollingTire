e6766d47a9fe8b7883bc29660d574ae64aa2129e
cho <63055734+CHOYOOBUM@users.noreply.github.com>
2020-10-18 08:19:57 +0900

    맵 자동 생성
    맵 자동 생성

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+
+    private Transform PlayerTransform;
+    private float spawnZ = 0.0f;
+    private float tileLength = 10.0f; // 타일 크기
+    private int amnTilesOnScreen = 3; //새로 생기는 타일의 개수
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+       
+        for(int i =0; i < amnTilesOnScreen; i++)
+        {
+            SpawnTile();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(PlayerTransform.position.z > (spawnZ - amnTilesOnScreen * tileLength))
+        {
+            SpawnTile();
+        }
+    }
+
+    private void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject go;
+        go = Instantiate(tilePrefabs[0]) as GameObject;
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
+    }
+}
