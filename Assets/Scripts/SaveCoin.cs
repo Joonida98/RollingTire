683d41ed03470bffa4b168d3ef8c06cb649bb822
cho <63055734+CHOYOOBUM@users.noreply.github.com>
2020-11-29 06:25:12 +0900

    도시맵 제작 완료
    새로운 씬에 도시맵 제작 완료
    문제점 : 다른씬에다가 작업을 하면 코인을 불러와야되는데 이점이 까다롭다..

---
+++ Assets/Scripts/SaveCoin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveCoin : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
