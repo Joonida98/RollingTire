06fff210d5f2c38df493a1e6ba84abc146f01ac2
LJH <joonida98@gmail.com>
2020-12-10 03:28:12 +0900

    gpgs설정 12.10-2

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "670871648854"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "670871648854-hj3rk4u2a6q336rleu4t1a52ljkpe252.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
---
+++ Assets/Scripts/GPGSManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+
+public class GPGSManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()      
+        .RequestEmail()
+        // requests a server auth code be generated so it can be passed to an
+        //  associated back end server application and exchanged for an OAuth token.
+        .RequestServerAuthCode(false)
+        // requests an ID token be generated.  This OAuth token can be used to
+        //  identify the player to other services such as Firebase.
+        .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
+
+        Login();
+    }
+
+    // Update is called once per frame
+    void Login()
+    {
+        if (Social.localUser.authenticated)
+        {
+            Debug.Log("이미 로그인된 유저");
+        }
+        else
+        {
+            Social.localUser.Authenticate((bool success) =>
+                {
+                    if (success)
+                    {
+                        Debug.Log("로그인 성공");
+                    }
+                    else
+                    {
+                        Debug.Log("로그인 실패");
+                    }
+                });
+        }
+    }
+}
